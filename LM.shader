4ee570d45a6755ee26064144d4f71e35e349e236
jackie2009 <jackie_2009@163.com>
2020-03-02 23:52:13 +0800

    实现简单体积GI测试

---
+++ LM.shader
@@ -0,0 +1,62 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "PengLu/Unlit/TextureLM" {
+Properties {
+	_MainTex ("Base (RGB)", 2D) = "white" {}
+	_SHLightingScale("LightProbe influence scale",float) = 1
+}
+ 
+SubShader {
+	Tags { "Queue"="Geometry""LightMode"="ForwardBase""RenderType"="Opaque"   }
+	LOD 100
+	
+	Pass {  
+		CGPROGRAM
+ 
+ #pragma target 3.5
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+ 
+		
+ 
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				float3 normal : NORMAL;
+				half2 texcoord : TEXCOORD0;
+				float3  worldPos : TEXCOORD1;
+				UNITY_FOG_COORDS(2)
+			};
+// uniform float4 allLPData[800]; 
+       StructuredBuffer <float4> allLPData;
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _SHLightingScale;
+			   sampler3D _VolumeGITex;
+			v2f vert (appdata_base v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+				 
+				o.worldPos = mul(UNITY_MATRIX_M, v.vertex);
+	            o.normal=UnityObjectToWorldNormal(v.normal);
+			
+							 
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				 
+				 fixed4 col = tex3D(_VolumeGITex, i.worldPos/10);
+				
+				return   (max(0,i.normal.z*col.r)+max(0,i.normal.z*col.b)+max(0,-i.normal.x*col.g)+max(0,-i.normal.x*col.a)) *_SHLightingScale;
+			}
+		ENDCG
+	}
+}
+}
---
+++ LightProbeMake.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightProbeMake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture3D volumeGI;
+    void Start()
+    {
+         
+    }
+    float numin(Color c)
+    {
+		
+        return Vector3.Dot(new Vector3(0.22f, 0.707f, 0.071f), new Vector3(c.r,c.g,c.b));
+    }
+    // Update is called once per frame
+    [ContextMenu("makeText3d")]
+    void makeText3d()
+    {
+        Dictionary<int,int> posIndexs=new Dictionary<int,int>();
+        for (int i = 0; i <  LightmapSettings.lightProbes.positions.Length; i++)
+        {
+            var pos = LightmapSettings.lightProbes.positions[i];
+           int index= ((int) pos.z) * 100 + ((int) pos.y) * 10 + (int) pos.x;
+           posIndexs[index] = i;
+        }
+         volumeGI=new Texture3D(10,10,10,TextureFormat.ARGB32,false);
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int z = 0; z < 10; z++)
+                {
+                    var pos =new Vector3(x+0.5f,y+0.5f,z+0.5f);
+                    int index= ((int) pos.z) * 100 + ((int) pos.y) * 10 + (int) pos.x;
+                 var sh=   LightmapSettings.lightProbes.bakedProbes[  posIndexs[index] ];
+                 Color [] colors=new Color[4];
+                 sh.Evaluate(new Vector3[]{Vector3.forward, Vector3.left, Vector3.back, Vector3.right}, colors);
+                 Color clr=new Color(numin(colors[0]),numin(colors[1]),numin(colors[2]),numin(colors[3]));
+                   volumeGI.SetPixel(x,y,z,clr);
+                }
+                
+            }
+        }
+        
+        volumeGI.Apply();
+        Shader.SetGlobalTexture("_VolumeGITex",volumeGI);
+    }
+
+    [ContextMenu("makeLP")]
+    void makeLP()
+    {
+        var lpg=GetComponent<LightProbeGroup>();
+        var list=new List<Vector3>();
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int z = 0; z < 10; z++)
+                {
+                    list.Add(new Vector3(x+0.5f,y+0.5f,z+0.5f));
+                    
+                }
+                
+            }
+        }
+
+        lpg.probePositions = list.ToArray();
+    }
+
+    [ContextMenu("switchGIEnable")]
+    void switchGIEnable()
+    {
+        Shader.SetGlobalFloat("_VolumeGIUsed",1- Shader.GetGlobalFloat("_VolumeGIUsed"));
+
+    }
+}
